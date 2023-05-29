00c2db09065e90507a37d74f8cdd660b831f009a
unknown <maxadc@126.com>
2023-05-29 16:06:18 +0800

    1

--- 搜索引擎切换.js
+++ 搜索引擎切换.js
@@ -1,16 +1,26 @@
 // ==UserScript==
 // @name         搜索引擎切换
-// @namespace    https://your-website-here.com/
+// @namespace    https://github.com/maxadc/tampermonkey/blob/main/%E6%90%9C%E7%B4%A2%E5%BC%95%E6%93%8E%E5%88%87%E6%8D%A2.js
 // @version      1
-// @description  Redirects from outbound links via specific targets.
-// @author       Your Name Here
+// @description  搜索引擎切换
+// @author       ant
 // @match        http*://cn.bing.com/search?q=*
 // @match        http*://www.baidu.com/s?wd=*
-// @match        http*://www.google.com/search?q=*
+// @match      http*://*.google.co.jp/search?q=*
+// @match      http*://*.google.com/search?q=*
+// @match      http*://www.chinaso.com/newssearch/all/allResults?q=*
+// @match        http*://*.duckduckgo.com/*
 // @grant        GM_registerMenuCommand
 // @run-at1       document-start
 // ==/UserScript==
 
+/**
+ * 使用以及自己添加还是比较简单,按照格式来就行.需要注意的是,如果是添加自己的搜索引擎,
+ * 许要修改 //@match 按照格式添加,*是通配符,不能添加到域的后缀上.
+ * 特别是谷歌搜索引擎需要注意,如果常用的不是国际版或日本版记得增加自己的域名
+ * 另外给"中国搜索"打个广告,
+ * 中国搜索是中国官方推出的搜索引擎,主打干净,没有广告,没有任何非法内容,适合小孩子用.
+ */
 let SiteConfig = {//网站规则配置
     baidu: {  // 百度
         webUrlReg: /https*:\/\/www\.baidu\.com\/s\?wd=([^\s&]+)/,
@@ -26,6 +36,11 @@ let SiteConfig = {//网站规则配置
         webUrlReg: /https*:\/\/www\.google\..{3,5}?\/search\?q=([^\s&]+)/,
         name: "日本google",
         searchUrl: "https://www.google.co.jp/search?q="
+    },
+    chinaso: {  // 中国搜索
+        webUrlReg: /https*:\/\/www\.chinaso\.com\/newssearch\/all\/allResults\?q=([^\s&]+)/,
+        name: "中国搜索",
+        searchUrl: "https://www.chinaso.com/newssearch/all/allResults?q="
     },
     duckduckgo: {  // duckduckgo
         webUrlReg: /https*:\/\/html\.duckduckgo\.com\/html\/\?q=([^\s&]+)/,
